5c0adbdcaae60a2145548d16dd03b271c5baf8ed
v123456k2 <56814804+v123456k2@users.noreply.github.com>
2020-01-26 17:49:02 -0500

    updated map markers

--- ionic-foodshed/src/app/map/map.page.ts
+++ ionic-foodshed/src/app/map/map.page.ts
@@ -20,18 +20,25 @@ export class MapPage implements OnInit {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYWVjaGFuIiwiYSI6ImNqbXd4ZmYzYTA0eWcza3J0NzVsNnNkcWoifQ.PQuBcFIs9STCQ6uf8DrJNw'; // token from previous group
     this.map = new mapboxgl.Map({ // initialize new map object
       style: 'mapbox://styles/aechan/cjmwxodn95lir2rmoq60ydb3m', // style defined by previous group
-      center: [-89.4125, 43.0766], // center lat and longitude
+      //center: [-89.4125, 43.0766], // center lat and longitude
+      center: [-89.405424, 43.073862], // adjusted center lat and longitude
       zoom: 15,
       pitch: 0,
       minZoom: 1, // restrict map zoom - buildings not visible beyond 13
-      maxZoom: 17,
+      maxZoom: 18,
       container: 'map'
     });
     // add a single marker
+    //var marker = new mapboxgl.Marker()
+    //  .setLngLat([-89.4125, 43.0766])
+    //  .addTo(this.map);
     var marker = new mapboxgl.Marker()
-      .setLngLat([-89.4125, 43.0766])
+      .setLngLat([-89.412213, 43.074975])
       .addTo(this.map);
-      
+
+    var marker2 = new mapboxgl.Marker()
+      .setLngLat([-89.398564, 43.072439])
+      .addTo(this.map);  
 
 
   }
