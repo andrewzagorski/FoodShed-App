69c45acb707565985783f328e40d40d55fba3e81
Avery2 <JustinAveryChan@gmail.com>
2020-01-26 14:03:32 -0600

    undo bad commit, add storage imports

--- ionic-foodshed/src/app/app.module.ts
+++ ionic-foodshed/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-// import { IonicStorageModule } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage';
 import { HttpClientModule } from '@angular/common/http';
 
 import { Camera } from '@ionic-native/Camera/ngx';
@@ -21,7 +21,7 @@ import { LocalstorageService } from './localstorage.service';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
-    // IonicStorageModule.forRoot()
+    IonicStorageModule.forRoot()
   ],
   providers: [
     StatusBar,
